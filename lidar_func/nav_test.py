12c26abe56403485d174c680918c01e4010da335
Robert Schneider <robert@mi-s.de>
2023-05-03 22:10:04 +0200

    follow online tutorial

---
+++ lidar_func/nav_test.py
@@ -0,0 +1,54 @@
+import time
+from copy import deepcopy
+
+from geometry_msgs.msg import PoseStamped
+from rclpy.duration import Duration
+import rclpy
+
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
+
+def main():
+    rclpy.init()
+
+    navigator = BasicNavigator()
+
+    initial_pose = PoseStamped()
+    initial_pose.header.frame_id = 'map'
+    initial_pose.header.stamp = navigator.get_clock().now().to_msg()
+    initial_pose.pose.position.x = 3.45
+    initial_pose.pose.position.y = 2.15
+    initial_pose.pose.orientation.z = 1.0
+    initial_pose.pose.orientation.w = 0.0
+    navigator.setInitialPose(initial_pose)
+
+    navigator.waitUntilNav2Active()
+
+    goal_pose = PoseStamped()
+    goal_pose.header.frame_id = 'map'
+    goal_pose.header.stamp = navigator.get_clock().now().to_msg()
+    goal_pose.pose.position.x = 3.0
+    goal_pose.pose.position.y = 2.0
+    goal_pose.pose.orientation.z = 1.0
+    goal_pose.pose.orientation.w = 0.0
+    print("Reseived coords")
+    navigator.goToPose(goal_pose)
+
+    i = 0
+    while not navigator.isTaskComplete():
+        i += 1
+        feedback = navigator.getFeedback()
+        if feedback and i % 5 == 0:
+            print("Estimated time of arrival: {0:.0f} seconds.".format(Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9))
+
+    result = navigator.getResult()
+    if result == TaskResult.SUCCEEDED:
+        print("Goal succeeded!")
+    elif result == TaskResult.CANCELED:
+        print("Goal was canceled!")
+    elif result == TaskResult.FAILED:
+        print("Goal failed!")
+
+    exit(0)
+
+if __name__ == '__main__':
+    main()
