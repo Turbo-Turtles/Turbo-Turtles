faf43518893e29e7a4178d56b2020e33bb05032f
Robert Schneider <robert@mi-s.de>
2023-05-07 17:38:11 +0200

    add launch files

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'lidar_func'
@@ -10,12 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        #('share/' + package_name + '/launch', ['launch/', '*launch.[pxy][yma]*']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.lua*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='robert',
     maintainer_email='robert@mi-s.de',
-    description='TODO: Package description',
+    description='Disposable test environment for everything related to the LiDAR',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
