0273744accc7a2406505172c44573cd765d7b1b9
Robert Schneider <robert@mi-s.de>
2023-05-08 15:53:26 +0200

    create config directory

---
+++ lidar_navigation/launch/navigation.launch.py
@@ -0,0 +1,28 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    config_dir = os.path.join(get_package_share_directory('lidar_navigation'),'config')
+    map_file = os.path.join(config_dir, 'tb3_map.yaml')
+
+    return LaunchDescription([
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                get_package_share_directory('turtlebot3_gazebo'),
+                '/launch',
+                '/turtlebot3_world.launch.py'
+            ])
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2_node',
+            output='screen'
+        )
+    ])
--- lidar_navigation/setup.py
+++ lidar_navigation/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'lidar_navigation'
@@ -10,12 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        #('share/' + package_name + '/launch', ['launch/', '*launch.[pxy][yma]*']),
+        #(os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        #(os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Robert Schneider',
     maintainer_email='robert@mi-s.de',
-    description='TODO: Package description',
+    description='Reads inputs from LiDAR scanner and uses this information to navigate.',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
