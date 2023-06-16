a13c1fe46f4ea6c330ea1d67c9cf5fc9ce7112b0
pi-tronic <robert@mi-s.de>
2023-06-16 16:23:59 +0200

    i give up bruuuhhhhh

--- lidar_navigation/lidar_navigation/position_listener.py
+++ lidar_navigation/lidar_navigation/position_listener.py
@@ -1,10 +1,21 @@
 #! /usr/bin/env python3
+##################################
+#
+# Returns the current position of the robot
+#
+# ros2 service call /current_position turtlebot3_interfaces/srv/GetPosition "{request: True}"
+#
+##################################
+
+import sys
+sys.path.insert(0, '')
+
 import rclpy
 from rclpy.node import Node
 
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
-from turtlebot3_interfaces.srv import GetPosition
+# from turtlebot3_interfaces.srv import GetPosition
 
 # classes start
 
@@ -25,12 +36,11 @@ class PositionListener(Node):
             qos_profile=qos_policy,
         )
 
-        self.position_service_ = self.create_service(
-            GetPosition,
-            'current_position',
-            self.service_callback,
-            qos_profile=qos_policy,
-        )
+        # self.position_service_ = self.create_service(
+        #     GetPosition,
+        #     'current_position',
+        #     self.service_callback,
+        # )
 
     def listener_callback(self, msg):
         self.pos.pose.position.x = msg.pose.pose.position.x
@@ -38,8 +48,32 @@ class PositionListener(Node):
         self.pos.pose.orientation.z = msg.pose.pose.orientation.z
         self.pos.pose.orientation.w = msg.pose.pose.orientation.w
 
-    def service_callback(self, request, response):
-        self.get_logger().info("Sending current position...")
-        response.position = self.pos
+        # print(self.pos.pose.position.x)
+        # print(self.pos.pose.position.y)
+
+    # def service_callback(self, request: GetPosition.Request, response: GetPosition.Response):
+    #     self.get_logger().info("Sending current position...")
+    #     response.position = self.pos
+
+    #     print(self.pos.pose.position.x)
+    #     print(self.pos.pose.position.y)
         
-        return response
+    #     return response
+
+    def get_position(self):
+        return self.pos
+
+
+# main
+# def main():
+#     rclpy.init()
+#     node = PositionListener()
+#     rclpy.spin(node)
+#     node.destroy_node()
+#     rclpy.shutdown()
+
+#     exit(0)
+
+
+# if __name__ == '__main__':
+#     main()
