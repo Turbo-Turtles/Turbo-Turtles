cc1c1a8f26f1b43eea32ad20925b52fddc8ba177
oreys <georg.fforberger@gmail.com>
2023-06-20 14:01:46 +0200

    improfed lanedetection

--- lane_detection/lane_detection/lane_det_3.py
+++ lane_detection/lane_detection/lane_det_3.py
@@ -52,25 +52,25 @@ class LaneDetectionNode(Node):
     cv2.setTrackbarPos('Val_yellow_max','yellow_threshold', 235)
     """
 
-
-    """
-    # Set default value for HSV trackbars.
-    cv2.setTrackbarPos('Hue_white_min', 'white_threshold', 55)
-    cv2.setTrackbarPos('Hue_white_max', 'white_threshold', 120)
+    
+    # Set default value for HSV trackbars.  :: Rosbag: Turbo-Turtles_record_05
+    cv2.setTrackbarPos('Hue_white_min', 'white_threshold', 0)
+    cv2.setTrackbarPos('Hue_white_max', 'white_threshold', 179)
     cv2.setTrackbarPos('Sat_white_min', 'white_threshold', 0)
-    cv2.setTrackbarPos('Sat_white_max', 'white_threshold', 70)
-    cv2.setTrackbarPos('Val_white_min', 'white_threshold', 190)
+    cv2.setTrackbarPos('Sat_white_max', 'white_threshold', 40)
+    cv2.setTrackbarPos('Val_white_min', 'white_threshold', 210)
     cv2.setTrackbarPos('Val_white_max', 'white_threshold', 255)
 
-    cv2.setTrackbarPos('Hue_yellow_min','yellow_threshold', 20)
-    cv2.setTrackbarPos('Hue_yellow_max','yellow_threshold', 65)
-    cv2.setTrackbarPos('Sat_yellow_min','yellow_threshold', 40)
-    cv2.setTrackbarPos('Sat_yellow_max','yellow_threshold', 140)
-    cv2.setTrackbarPos('Val_yellow_min','yellow_threshold', 90)
-    cv2.setTrackbarPos('Val_yellow_max','yellow_threshold', 230)
-    """
+    cv2.setTrackbarPos('Hue_yellow_min','yellow_threshold', 25)
+    cv2.setTrackbarPos('Hue_yellow_max','yellow_threshold', 50)
+    cv2.setTrackbarPos('Sat_yellow_min','yellow_threshold', 55)
+    cv2.setTrackbarPos('Sat_yellow_max','yellow_threshold', 190)
+    cv2.setTrackbarPos('Val_yellow_min','yellow_threshold', 105)
+    cv2.setTrackbarPos('Val_yellow_max','yellow_threshold', 225)
+    
 
-    # Set default value for HSV trackbars.
+    """
+    # Set default value for HSV trackbars.  :: GAZEBO-Simulation
     cv2.setTrackbarPos('Hue_white_min', 'white_threshold', 10)
     cv2.setTrackbarPos('Hue_white_max', 'white_threshold', 4)
     cv2.setTrackbarPos('Sat_white_min', 'white_threshold', 0)
@@ -84,6 +84,26 @@ class LaneDetectionNode(Node):
     cv2.setTrackbarPos('Sat_yellow_max','yellow_threshold', 255)
     cv2.setTrackbarPos('Val_yellow_min','yellow_threshold', 150)
     cv2.setTrackbarPos('Val_yellow_max','yellow_threshold', 255)
+    """
+
+
+    '''
+    # Set default value for HSV trackbars.  :: Rosbag: Turbo-Turtles_TurboTurtles
+    cv2.setTrackbarPos('Hue_white_min', 'white_threshold', 0)
+    cv2.setTrackbarPos('Hue_white_max', 'white_threshold', 179)
+    cv2.setTrackbarPos('Sat_white_min', 'white_threshold', 0)
+    cv2.setTrackbarPos('Sat_white_max', 'white_threshold', 20)
+    cv2.setTrackbarPos('Val_white_min', 'white_threshold', 220)
+    cv2.setTrackbarPos('Val_white_max', 'white_threshold', 255)
+
+    cv2.setTrackbarPos('Hue_yellow_min','yellow_threshold', 30)
+    cv2.setTrackbarPos('Hue_yellow_max','yellow_threshold', 40)
+    cv2.setTrackbarPos('Sat_yellow_min','yellow_threshold', 130)
+    cv2.setTrackbarPos('Sat_yellow_max','yellow_threshold', 255)
+    cv2.setTrackbarPos('Val_yellow_min','yellow_threshold', 110)
+    cv2.setTrackbarPos('Val_yellow_max','yellow_threshold', 255)
+    '''
+
 
 
     # create trackbars for image cropping
@@ -94,7 +114,7 @@ class LaneDetectionNode(Node):
     cv2.createTrackbar('p4_x','crop_image',0,100,nothing)
 
     # Set default value for image cropping trackbars.
-    cv2.setTrackbarPos('p12_y', 'crop_image', 60)
+    cv2.setTrackbarPos('p12_y', 'crop_image', 70)
     cv2.setTrackbarPos('p2_x', 'crop_image', 100)
     cv2.setTrackbarPos('p4_x', 'crop_image', 100)
 
@@ -102,51 +122,38 @@ class LaneDetectionNode(Node):
 
     def __init__(self):
         super().__init__("lane_detection_node_3")
-        self.subscription = self.create_subscription(Image, "/camera/image_raw", self.image_callback, 1) #create image subscriber
         self.cv_bridge = CvBridge()
 
-        self.publisher = self.create_publisher(Twist, "cmd_vel", 1) #create movement value publisher
+        self.sub_image_original = self.create_subscription(CompressedImage, "/image_raw/compressed", self.image_callback, 1) #create image subscriber
+
+        self.pub_vel_cmd = self.create_publisher(Twist, "cmd_vel", 1) #create movement value publisher
+        self.pub_image_lane = self.create_publisher(CompressedImage, "/lane_image_out", 1) #create lane image publisher
 
     
-    def image_callback(self, msg):
-        cv_image = self.cv_bridge.imgmsg_to_cv2(msg, "bgr8")
+    def image_callback(self, img_msg):
+        self.cv_image = self.cv_bridge.compressed_imgmsg_to_cv2(img_msg, "bgr8")
         #__+__cv2.imshow("origianl", cv_image)
-        self.get_logger().info("Image recived")
+        #__+__self.get_logger().info("Image recived")
 
-        croped = self.crop_image(cv_image)
-        result, white_lane, yellow_lane = self.detect_lanes(croped)
+        height, width = self.cv_image.shape[:2]
+        self.window_width = width
+        self.window_height = height
+        self.window_size = width * height
+
+        #__+__self.get_logger().info(str(height)+ ","+ str(width))
+
+        croped_img = self.crop_image(self.cv_image)
+
+        result, white_lane, yellow_lane = self.detect_lane(croped_img)
 
         # Display the processed image (for visualization purposes)
         #__+__cv2.imshow("Lane_Detection_Result", result)
         cv2.waitKey(1)
 
+        self.driving(white_lane, yellow_lane, result)
     
-        # Process lane detection data and calculate desired steering angle and velocity
-
+        '''# Process lane detection data and calculate desired steering angle and velocity
         if white_lane is not None and yellow_lane is not None:
-            # Calculate the center of the lane
-            lane_center = ((white_lane[0][0] + yellow_lane[0][0]) // 2, (white_lane[1][1] + yellow_lane[1][1]) // 2)
-
-            # Calculate the deviation from the center of the image
-            image_center = cv_image.shape[1] / 2
-            deviation = lane_center[0] - image_center
-
-            # Define control parameters
-            kp = 0.1  # Proportional gain
-            velocity = 0.05  # Constant velocity
-
-            # Calculate the desired steering angle based on the deviation and control parameters
-            steering_angle = kp * deviation / 500      #<------- winkel zu klein und zu groß raußfiltern
-            self.get_logger().info(str(steering_angle))
-
-            # Create Twist message with desired linear and angular velocities
-            twist_msg = Twist()
-            twist_msg.linear.x = velocity
-            twist_msg.angular.z = steering_angle
-
-            # Publish the twist message to control the TurtleBot3
-            self.publisher.publish(twist_msg)
-
             # Calculate the center lane coordinates
             w1, w2 = white_lane
             y1, y2 = yellow_lane
@@ -156,16 +163,20 @@ class LaneDetectionNode(Node):
             center_lane_y = int(w2[1] + y2[1])
 
             # Draw the center lane on the image
-            cv2.line(result, (center_lane_x_2, center_lane_y), (center_lane_x_1, cv_image.shape[1]), (0, 0, 255), 5)
+            cv2.line(result, (center_lane_x_2, center_lane_y), (center_lane_x_1, self.window_height), (0, 0, 255), 5)
 
             # Display the image with the lanes
+            self.pub_image_lane.publish(self.cv_bridge.cv2_to_compressed_imgmsg(result, "jpg"))
             cv2.imshow("Lane Detection driving line", result)
             cv2.waitKey(1)
+        else:
+            self.pub_image_lane.publish(self.cv_bridge.cv2_to_compressed_imgmsg(result, "jpg"))
+            cv2.imshow("Lane Detection driving line", result)
+            cv2.waitKey(1)'''
+
 
 
     def crop_image(self, image):
-        height, width = image.shape[:2]
-
         p1_x = cv2.getTrackbarPos('p1_x', 'crop_image')
         p2_x = cv2.getTrackbarPos('p2_x', 'crop_image')
         p12_y = cv2.getTrackbarPos('p12_y', 'crop_image')
@@ -176,14 +187,14 @@ class LaneDetectionNode(Node):
     
         # Locate points of the documents
         # or object which you want to transform
-        pts1 = np.float32([[((width * p1_x)/100), ((height * p12_y)/100)], [((width * p2_x)/100), ((height * p12_y)/100)],
-                           [((width * p3_x)/100),   height],               [((width * p4_x)/100),   height]])
-        pts2 = np.float32([[0, 0],   [width, 0],
-                           [0, height], [width, height]])
+        pts1 = np.float32([[((self.window_width * p1_x)/100), ((self.window_height * p12_y)/100)], [((self.window_width * p2_x)/100), ((self.window_height * p12_y)/100)],
+                           [((self.window_width * p3_x)/100),   self.window_height],               [((self.window_width * p4_x)/100),   self.window_height]])
+        pts2 = np.float32([[0, 0],   [self.window_width, 0],
+                           [0, self.window_height], [self.window_width, self.window_height]])
         
         # Apply Perspective Transform Algorithm
         matrix = cv2.getPerspectiveTransform(pts1, pts2)
-        result = cv2.warpPerspective(image, matrix, (width, height))
+        result = cv2.warpPerspective(image, matrix, (self.window_width, self.window_height))
         
         # Wrap the transformed image
         cv2.imshow('crop_image', result) # Transformed Capture
@@ -191,28 +202,26 @@ class LaneDetectionNode(Node):
         return result
 
     
-    def detect_lanes(self, image):
+    def detect_lane(self, image):
         # find White and Yellow Lines
         white_line = self.maskWhiteLine(image)
         yellow_line = self.maskYellowLine(image)
 
-        height = image.shape[0]
-
         # Fit a line to each set of lane lines (left and right)
-        white_lane = self.fit_lane_line(white_line, height)
-        yellow_lane = self.fit_lane_line(yellow_line, height)
+        white_lane_line = self.fit_lane_line(white_line)
+        yellow_lane_line = self.fit_lane_line(yellow_line)
 
         # Create a blank image to draw the lanes on
         lane_image = np.zeros_like(image)
 
         # Draw the left and right lanes on the lane image
-        self.draw_lane_line(lane_image, white_lane)
-        self.draw_lane_line(lane_image, yellow_lane)
+        self.draw_lane_line(lane_image, white_lane_line)
+        self.draw_lane_line(lane_image, yellow_lane_line)
 
         # Combine the lane image with the original image
         result = cv2.addWeighted(image, 0.7, lane_image, 1, 1)
 
-        return result, white_lane, yellow_lane
+        return result, white_lane_line, yellow_lane_line
         #pass
 
 
@@ -251,12 +260,12 @@ class LaneDetectionNode(Node):
         #__+__cv2.imshow("Canny edge detection_w", edges)
 
         # Define a region of interest (ROI)
-        height, width = edges.shape[:2]
         roi_vertices = [
-            (width * 0.2, 0),
-            (width * 0.9, 0),
-            (width, height),
-            (width * 0.8, height)
+            (self.window_width * 0.2, 0),
+            #(self.window_width * 0.9, 0),
+            (self.window_width * 1.0, 0),
+            (self.window_width, self.window_height),
+            (self.window_width * 0.8, self.window_height)
         ]
         mask = np.zeros_like(edges)
         cv2.fillPoly(mask, np.array([roi_vertices], dtype=np.int32), 255)
@@ -274,15 +283,21 @@ class LaneDetectionNode(Node):
         )
 
         w_lines = []
-
         try:
             for line in lines:
                 x1, y1, x2, y2 = line[0]
                 slope = (y2 - y1) / (x2 - x1 + 1e-6)  # Avoid division by zero
-                if slope > 0.5:         # maybe change parameter
+                if slope > -0.5:         # maybe change parameter
                     w_lines.append(line[0])
         except:
             w_lines = []
+        
+        '''w_lines = []
+        try:
+            for line in lines:
+                    w_lines.append(line[0])
+        except:
+            w_lines = []'''
 
         return w_lines
     
@@ -322,12 +337,12 @@ class LaneDetectionNode(Node):
         #__+__cv2.imshow("Canny edge detection_y", edges)
 
         # Define a region of interest (ROI)
-        height, width = edges.shape[:2]
         roi_vertices = [
-            (width * 0.1, 0),
-            (width * 0.8, 0),
-            (width * 0.2, height),
-            (0, height)
+            #(self.window_width * 0.1, 0),
+            (self.window_width * 0, 0),
+            (self.window_width * 0.8, 0),
+            (self.window_width * 0.2, self.window_height),
+            (0, self.window_height)
         ]
         mask = np.zeros_like(edges)
         cv2.fillPoly(mask, np.array([roi_vertices], dtype=np.int32), 255)
@@ -349,15 +364,22 @@ class LaneDetectionNode(Node):
             for line in lines:
                 x1, y1, x2, y2 = line[0]
                 slope = (y2 - y1) / (x2 - x1 + 1e-6)  # Avoid division by zero
-                if slope < -0.5:         # maybe change parameter
+                if slope < 0.5:         # maybe change parameter
                     y_lines.append(line[0])
         except:
             y_lines = []
+        
+        '''y_lines = []
+        try:
+            for line in lines:
+                y_lines.append(line[0])
+        except:
+            y_lines = []'''
 
         return y_lines
 
 
-    def fit_lane_line(self, lines, height):
+    def fit_lane_line(self, lines):
         if len(lines) == 0:
             return None
 
@@ -370,8 +392,8 @@ class LaneDetectionNode(Node):
         coeffs = np.polyfit(np.squeeze(x), np.squeeze(y), 1)
 
         # Calculate the start and end points of the lane line
-        y1 = height
-        y2 = int(height * 0.3)
+        y1 = self.window_height
+        y2 = int(self.window_height * 0.3)
         x1 = int((y1 - coeffs[1]) / coeffs[0])
         x2 = int((y2 - coeffs[1]) / coeffs[0])
 
@@ -381,6 +403,83 @@ class LaneDetectionNode(Node):
     def draw_lane_line(self, image, lane):
         if lane is not None:
             cv2.line(image, lane[0], lane[1], (255, 150, 0), 10)
+
+    
+    def driving(self, white_lane, yellow_lane, img):
+        # Define control parameters
+        kp = 0.02  # Proportional gain
+        max_vel = 0.1  # Constant velocity
+        velocity = max_vel
+        steering_angle = 0.0
+        self.old_deviation = 0
+        self.first = True
+
+        if white_lane is not None and yellow_lane is not None:
+            # Calculate the center of the lane
+            #__+__lane_center = ((white_lane[0][0] + yellow_lane[0][0]) // 2, (white_lane[1][1] + yellow_lane[1][1]) // 2)
+
+            w1, w2 = white_lane
+            y1, y2 = yellow_lane
+
+            center_lane_x_1 = int((w1[0] + y1[0]) / 2)
+            center_lane_x_2 = int((w2[0] + y2[0]) / 2)
+            center_lane_y = int(w2[1] + y2[1])
+
+            lane_center = (center_lane_x_2, center_lane_y)
+            center_lane = ((center_lane_x_2, center_lane_y),(center_lane_x_1, self.window_height))
+
+            # Calculate the deviation from the center of the image
+            image_center = self.window_width / 2
+            deviation = lane_center[0] - image_center
+
+            # Draw the center lane on the image
+            #__+__cv2.line(img, lane_center, (int(self.window_width/2), self.window_height), (0, 0, 255), 5)
+            cv2.line(img, center_lane[0], center_lane[1], (0, 0, 255), 5)
+
+            if self.first == True:
+                self.old_deviation = deviation
+                self.first = False
+
+            if abs(self.old_deviation - deviation) > 50:
+                deviation = self.old_deviation
+
+            # Calculate the desired steering angle based on the deviation and control parameters
+            if deviation < 100 and deviation > -100:
+                steering_angle = kp * -deviation
+                velocity = max_vel
+            elif deviation > 100:
+                steering_angle = -1.0
+                velocity = max_vel/(deviation*kp*2)
+            elif deviation < -100:
+                steering_angle = 1.0
+                velocity = -max_vel/(deviation*kp*2)
+
+        elif white_lane is None:
+            steering_angle = -0.2
+            velocity = max_vel/5
+
+        elif yellow_lane is None:
+            steering_angle = 0.2
+            velocity = max_vel/5
+
+        else:
+            steering_angle = 0.2
+            velocity = 0
+
+        # Display the image with the lanes
+        self.pub_image_lane.publish(self.cv_bridge.cv2_to_compressed_imgmsg(img, "jpg"))
+        cv2.imshow("Lane Detection driving line", img)
+        cv2.waitKey(1)
+        
+        self.get_logger().info('steer.: ' + str(steering_angle) + '; vel.: ' + str(velocity))
+
+        # Create Twist message with desired linear and angular velocities
+        twist_msg = Twist()
+        twist_msg.linear.x = velocity
+        twist_msg.angular.z = steering_angle
+
+        # Publish the twist message to control the TurtleBot3
+        self.pub_vel_cmd.publish(twist_msg)
     
 
 
